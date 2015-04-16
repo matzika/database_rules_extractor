019f461adb160c1f17b68f475c34da77e74ad828
cswatt <ceciliawatt@gmail.com>
2015-04-16 16:52:07 -0400

    progress on CSVDataHandler

--- database_rules_extractor/src/CSVDataHandler.java
+++ database_rules_extractor/src/CSVDataHandler.java
@@ -16,11 +16,9 @@ import au.com.bytecode.opencsv.*;
  * and its row in each table should correspond to the related row to the other tables
  */
 public class CSVDataHandler {
-	String filename;
-	static CDataCacheContainer container;
+	static CDataCacheContainer accidents_table;
 
 	public CSVDataHandler(String filename){
-		this.filename = filename;
 		try {
 			createTable();
 			readFile(filename);
@@ -30,21 +28,31 @@ public class CSVDataHandler {
 		} 
 	}
 
-	public static void createTable() throws CDataGridException{
+	/**
+	 * Just creates a table with the desired column names, their appropriate types.
+	 * @throws CDataGridException
+	 */
+	public void createTable() throws CDataGridException{
 		String[] columnNames = new String[]{"date", "borough", "zipcode", "persons_injured", "persons_killed", "pedestrians_injured", "pedestrians_killed",
 				"cyclicsts_injured", "cyclists_killed", "motorists_injured", "motorists_killed", "factor1", "factor2", "factor3", "factor4", "factor5",
 				"vehicle1", "vehicle2", "vehicle3", "vehicle4", "vehicle5"};
 		Class[] columnTypes = new Class[] {Date.class, String.class, String.class, Integer.class, Integer.class, Integer.class, Integer.class,
 				Integer.class, Integer.class, Integer.class, Integer.class, String.class, String.class, String.class, String.class, String.class,
 				String.class, String.class, String.class, String.class, String.class};
-		String[] primaryKeys = new String[]{"borough"};
+		//primary key for now is date, there's probably a better option
+		String[] primaryKeys = new String[]{"date"};
 		CRowMetaData metaDef = new CRowMetaData(columnNames, columnTypes, primaryKeys);
-		container = new CDataCacheContainer("mytest", metaDef, new HashMap());
+		accidents_table = new CDataCacheContainer("mytest", metaDef, new HashMap());
 	}
 	
-	
-
-	public static void readFile(String filename) throws CDataGridException, IOException {
+	/**
+	 * Uses CSVReader to read the file, creates an Accident object for each row. (where parsing happens)
+	 * Calls addRow on the created Accident.
+	 * @param filename
+	 * @throws CDataGridException
+	 * @throws IOException
+	 */
+	public void readFile(String filename) throws CDataGridException, IOException {
 		CSVReader reader = new CSVReader(new FileReader(filename));
 		String[] nextLine;
 		while ((nextLine = reader.readNext()) != null) {
@@ -53,17 +61,31 @@ public class CSVDataHandler {
 		}
 	}
 	
-	public static void addRow(Accident a) throws CDataGridException {
+	/**
+	 * Adds an accident's toRow method to the container.
+	 * @param a
+	 * @throws CDataGridException
+	 */
+	public void addRow(Accident a) throws CDataGridException {
 		CDataRow row = new CDataRow();
 		row.setRawData(a.toRow());
-		container.addData(new CDataRow[] {row});
+		accidents_table.addData(new CDataRow[] {row});
 	}
 	
-	public static void printTable() throws CDataGridException {
-		CDataRowSet results = container.getAll();
+	//this is mostly for checking stuff
+	public void printTable() throws CDataGridException {
+		CDataRowSet results = accidents_table.getAll();
 		System.out.println(results.toString());
 	}
 
+	/**
+	 * to get the accidents_table
+	 * @return
+	 */
+	public CDataCacheContainer getAccidentsTable(){
+		return accidents_table;
+	}
+	
 	// for testing
 	public static void main(String[] args){
 		String filename = "datafile_trunc.csv";
